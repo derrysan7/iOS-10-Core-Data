dc1acc4aa2821d9ebb839f463bb900f68bc4d61e
Derry Santoso <derrysantoso7@gmail.com>
2017-06-06 15:22:30 +0700

    process json from open weather

--- API Weather/API Weather/ViewController.swift
+++ API Weather/API Weather/ViewController.swift
@@ -13,6 +13,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=a98efa86379313cb8f8b3eab0c1abe26")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                
+                print(error)
+                
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                    
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    
+                        print(jsonResult)
+                        
+                    }catch{
+                        
+                        print("JSON Processing failed")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
