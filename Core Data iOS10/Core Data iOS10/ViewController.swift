93290a5506eaca82f9b0a84abce09ffb2b85c753
Derry Santoso <derrysantoso7@gmail.com>
2017-06-06 09:54:38 +0700

    Initial Commit

---
+++ Core Data iOS10/Core Data iOS10/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Core Data iOS10
+//
+//  Created by MacBookPro on 6/6/17.
+//  Copyright © 2017 Binary. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
