631da4a9682cd1f71baa981ca5ebee2f03c7e4b9
Derry Santoso <derrysantoso7@gmail.com>
2017-06-07 08:44:23 +0700

    Delete Function to delete row

--- Blog Reader/Blog Reader/MasterViewController.swift
+++ Blog Reader/Blog Reader/MasterViewController.swift
@@ -84,21 +84,6 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         return false
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let context = self.fetchedResultsController.managedObjectContext
-            context.delete(self.fetchedResultsController.object(at: indexPath))
-                
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
 
     func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
         cell.textLabel!.text = event.timestamp!.description
